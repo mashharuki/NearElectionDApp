1b211d9fe3c63de3cd898cc43b696a642a55a226
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-08-17 22:39:12 +0900

    nft_coreを実装する。

---
+++ near-election-dapp/backend/src/nft_core.rs
@@ -0,0 +1,26 @@
+use crate::*;
+
+// trait of NonFungibleTokenCore
+pub trait NonFungibleTokenCore {
+    // get nft info
+    fn nft_token(&self, token_id: TokenId) -> Option<JsonToken>;
+};
+
+#[near_bindgen]
+impl NonFungibleTokenCore for Contract {
+    // get token info
+    fn nft_token(&self, token_id: TokenId) -> Option<JsonToken> {
+
+        if let Some(token) = self.tokens_by_id.get(&token_id) {
+            // get metadata
+            let metadata = self.tokens_by_id.get(&token_id).unwrap();
+            // return
+            Some(JsonToken {
+                owner_id: token.owner_id,
+                metadata,
+            })
+        } else {
+            None
+        }
+    };
+};
